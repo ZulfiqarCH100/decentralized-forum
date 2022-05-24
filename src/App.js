ea9a5a7f8085e7d05d1e2d68234dc596fbad9f51
Zulfiqar <zulfiqarchkonoha@gmail.com>
2022-05-24 20:43:07 +0500

    Metamask login working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { account: "Please Log in with MetaMask" };
+    }
+
+    componentWillMount() {
+        this.loadBlockchainData();
+    }
+
+    async loadBlockchainData() {
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+        const accounts = await web3.eth.getAccounts();
+        this.setState({ account: accounts[0] });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello, World!</h1>
+                <p>Your account: {this.state.account}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
