82622e760a5ed24955f420fee79fa3289a618177
181184muhammadRahim <46868114+181184muhammadRahim@users.noreply.github.com>
2022-05-26 19:46:33 +0500

    Like and dislike functionality added

--- contract/contracts/forum.sol
+++ contract/contracts/forum.sol
@@ -2,31 +2,48 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract forum {
-    uint public count = 0;
+    uint256 public count = 0;
 
     struct Post {
-        uint id;
+        uint256 id;
         address owner;
         string message;
-        uint likes;
-        uint dislikes;
+        uint256 likes;
+        uint256 dislikes;
     }
 
-    mapping (uint => Post) public posts;
+    mapping(uint256 => Post) public posts;
 
-    
-    function makePost(string memory _message) public returns (uint) {
+    function makePost(string memory _message) public returns (uint256) {
         posts[count] = Post(count, msg.sender, _message, 0, 0);
         count++;
         return count;
     }
 
-    function getPosts() public view returns (Post[] memory){
-      Post[] memory id = new Post[](count);
-      for (uint i = 0; i < count; i++) {
-          Post storage post = posts[i];
-          id[i] = post;
-      }
-      return id;
-  }
+    function getPosts() public view returns (Post[] memory) {
+        Post[] memory id = new Post[](count);
+        for (uint256 i = 0; i < count; i++) {
+            Post storage post = posts[i];
+            id[i] = post;
+        }
+        return id;
+    }
+
+    function likePost(uint256 id) public returns (bool values) {
+        if (id >= 0 && id <= count) {
+            posts[id].likes += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function dislikePost(uint256 id) public returns (bool values) {
+        if (id >= 0 && id <= count) {
+            posts[id].dislikes += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
